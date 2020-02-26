595a76536dd6ced4dffca4f03ce1f4524c943b57
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-02-27 00:15:59 +0100

    struggling with setting a 2d rendering context at a global scope to be used from different function scopes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class AppComponent implements OnInit,OnDestroy {
   
   
     p.setup = () => {
-      p.createCanvas(p.windowWidth, 100).parent('experiment-canvas');
+      // p.createCanvas(p.windowWidth, 100).parent('experiment-canvas');
     };
   
   
--- src/app/walker-objectified/walker-objectified.component.ts
+++ src/app/walker-objectified/walker-objectified.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, OnDestroy, NgZone, HostBinding, HostListener } from '@angular/core';
+import { Component, AfterViewInit, OnInit, ViewChild, ElementRef, OnDestroy, NgZone, HostBinding, HostListener } from '@angular/core';
 import { Turtle } from '../turtle';
 import * as p5 from 'p5';
 
@@ -9,19 +9,28 @@ import * as p5 from 'p5';
   styleUrls: ['./walker-objectified.component.css'],
   styles: ['canvas { border-style: solid }']
 })
-export class WalkerObjectifiedComponent implements OnInit {
+export class WalkerObjectifiedComponent implements OnInit,AfterViewInit {
   @ViewChild('canvas', { static: true }) 
   canvas: ElementRef<HTMLCanvasElement>;
   private ctx: CanvasRenderingContext2D;
   private x; private y; private width; private height;
   private squares = [];
 
-  constructor(private ngZone: NgZone) {
+  constructor(private ngZone: NgZone, ctx?: CanvasRenderingContext2D ) {
     // ngZone.runOutsideAngular(() => this.animate());
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    console.log("CTX in animate() : ",ctx," This.ctx:",this.ctx);
+
+    this.animate(this.ctx);
  }
 
+ ngOnInit(){
+  this.ctx = this.canvas.nativeElement.getContext('2d');
+  console.log("ctx onInit:",this.ctx);
+  
+ }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
     // this.ctx.fillRect(this.x, this.y, this.width, this.height);
     // this.ctx.fillStyle = 'red';  
@@ -31,26 +40,30 @@ export class WalkerObjectifiedComponent implements OnInit {
       this.squares.push(new Square(this.ctx));
     }
     // this.animate();
-    this.ngZone.runOutsideAngular(() => this.animate());
+    // this.ngZone.runOutsideAngular(() => this.animate());
     
   }
   
 
-  animate() {  
+  animate(ctx?: CanvasRenderingContext2D) {  
+    // console.log("CTX in animate() : ",ctx," This.ctx:",this.ctx);
+    
     // this.ctx.fillStyle = 'red';  
     // const square = new Square(this.ctx);  
     
     // square.draw(5, 1, 25);  
     // this.squares[index].move(1, 30);
     
-    this.ctx.clearRect(0, 0, this.width, this.height);
-    setInterval(() => {
-      this.squares.forEach((square: Square) => {
-        square.moveRight();
-      });
-    }, 200);
+    // this.ctx.clearRect(0, 0, this.width, this.height);
+    // this.squares.forEach((square: Square) => {
+    //   square.moveRight();
+    // });
+    // setInterval(() => {
+     
+    // }, 200);
     
-    const id = requestAnimationFrame(this.animate);
+    // const id = requestAnimationFrame(this.animate);
+    // requestAnimationFrame(this.animate);
   }
 
 
@@ -69,7 +82,7 @@ export class Square {
 
   moveRight() {
     this.x++;
-      this.ctx.clearRect(this.z * this.x, this.z * this.y, this.z, this.z);
+    this.ctx.clearRect(this.z * this.x, this.z * this.y, this.z, this.z);
     this.draw();
   }
 
@@ -78,25 +91,5 @@ export class Square {
     this.ctx.strokeRect(this.z * this.x, this.z * this.y, this.z, this.z);
   }
 
-
-  // draw(x: number, y: number, z: number) {
-  //   this.ctx.fillRect(z * x, z * y, z, z);
-  //   this.ctx.strokeRect(z * x, z * y, z, z);
-  // }
-
-  // move(y: number, z: number) {
-  //   const max = this.ctx.canvas.width / z;
-  //   const canvas = this.ctx.canvas;
-  //   let x = 0;
-  //   const i = setInterval(() => {
-  //     this.ctx.clearRect(0, 0, canvas.width, canvas.height);      
-  //     this.draw(x, y, z);
-  //     x++;
-  //     if (x >= max) {
-  //       clearInterval(i);
-  //       // x = 0;
-  //     }
-  //   }, 200);    
-  // }
 
 }
