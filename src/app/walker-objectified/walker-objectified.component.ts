67ba98dae3265587f7c9636d74c37391405b4459
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-03-02 21:36:54 +0100

    setting up canvas 2

--- src/app/walker-objectified/walker-objectified.component.ts
+++ src/app/walker-objectified/walker-objectified.component.ts
@@ -7,7 +7,7 @@ import * as p5 from 'p5';
   selector: 'app-walker-objectified',
   templateUrl: './walker-objectified.component.html',
   styleUrls: ['./walker-objectified.component.css'],
-  styles: ['canvas { border-style: solid }']
+  styles: ['']
 })
 export class WalkerObjectifiedComponent implements OnInit, AfterViewInit {
   @ViewChild('canvas', { static: true }) 
@@ -18,21 +18,24 @@ export class WalkerObjectifiedComponent implements OnInit, AfterViewInit {
   private count = 0;
   private x; private y; 
   // setting a width and height for the canvas
-  @Input() public width = 400;
-  @Input() public height = 400;
+  @Input() public width; 
+  @Input() public height;
   
   private squares:Square[] = [];
 
-  constructor(private ngZone: NgZone) {}
+  constructor(private ngZone: NgZone) {
+
+  }
 
   ngAfterViewInit(){
+
     // get the context
     const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
     this.ctx = canvasEl.getContext('2d');
 
-    // set the width and height
-    canvasEl.width = this.width;
-    canvasEl.height = this.height;
+    // set canvas width and height
+    this.width = this.canvas.nativeElement.width;
+    this.height = this.canvas.nativeElement.height;
 
     // push squares into the squares array
     for (let index = 0; index < 3; index++) {
@@ -86,6 +89,8 @@ export class Square {
   private y = 0;
   private z = 3;
 
+  private square_width = 10;
+  private square_height = 10;
 
   private max;
   private min;
@@ -104,7 +109,7 @@ export class Square {
     
     this.max2 = 1;
     this.min2 = -1;
-    
+
   }
   
   
@@ -133,12 +138,12 @@ export class Square {
     }
 
 
-    if (this.x > this.width-40) {
-      this.x = this.width-40;
+    if (this.x > this.width-this.square_width) {
+      this.x = this.width-this.square_width;
     }
 
-    if (this.y > this.height-40) {
-      this.y = this.height-40;
+    if (this.y > this.height-this.square_height) {
+      this.y = this.height-this.square_height;
     }
 
     this.draw();
@@ -146,7 +151,7 @@ export class Square {
   
   draw() {
     this.ctx.fillStyle = 'red' //`rgba(${this.x},${this.y},${this.z},1)`;
-    this.ctx.fillRect(this.x, this.y, 40, 40);
+    this.ctx.fillRect(this.x, this.y, this.square_width, this.square_height);
     console.log('x: ',this.x, 'y: ',this.y);
     
   }
